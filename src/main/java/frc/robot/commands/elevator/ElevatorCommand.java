b40b339f129e7e60e91d66192158aab248139321
Alper1718 <104254640+Alper1718@users.noreply.github.com>
2024-12-27 21:58:09 +0300

    PID.

--- src/main/java/frc/robot/commands/elevator/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorCommand.java
@@ -9,30 +9,32 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem;
 
 public class ElevatorCommand extends Command {
   private final ElevatorSubsystem elevator;
+  private final double targetPosition;
 
-  public ElevatorCommand(ElevatorSubsystem elevator) {
+  public ElevatorCommand(ElevatorSubsystem elevator, double targetPosition) {
     this.elevator = elevator;
+    this.targetPosition = targetPosition;
     addRequirements(elevator);
   }
 
   @Override
   public void initialize() {
-    System.out.println("Elevator work");
+    System.out.println("Elevator PID start.");
   }
 
   @Override
   public void execute() {
-    elevator.Move(0.5);
+    elevator.moveToPosition(targetPosition);
   }
 
   @Override
   public void end(boolean interrupted) {
-    elevator.Move(0);
+    elevator.move(0);
     System.out.println("elevator end.");
   }
 
   @Override
   public boolean isFinished() {
-    return false;
+    return elevator.atTargetPosition();
   }
 }
