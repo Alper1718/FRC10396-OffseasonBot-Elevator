c39f9de6b66b1b7f75a740dda05206cf52499537
Alper1718 <104254640+Alper1718@users.noreply.github.com>
2024-12-27 21:57:02 +0300

    Update ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -8,24 +8,50 @@ package frc.robot.subsystems.elevator;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
   private final CANSparkFlex motor1;
   private final CANSparkFlex motor2;
+  private final PIDController pidController;
+  private final ElevatorSensorsIO sensors;
 
-  public ElevatorSubsystem(int motor1ID, int motor2ID, int CANcoderID) {
+  public ElevatorSubsystem(int motor1ID, int motor2ID, int CANcoderID, ElevatorSensorsIO sensors) {
     motor1 = new CANSparkFlex(motor1ID, MotorType.kBrushless);
     motor2 = new CANSparkFlex(motor2ID, MotorType.kBrushless);
 
     motor2.follow(motor1);
+
+    pidController = new PIDController(0.1, 0.0, 0.0); //To be calibrated...
+    pidController.setTolerance(0.05);
+    this.sensors = sensors;
   }
 
-  public void Move(double power){
+  public void moveToPosition(double targetPosition) {
+    double currentPosition = sensors.getNEOEncoderPosition();
+    double pidOutput = pidController.calculate(currentPosition, targetPosition);
+    motor1.set(pidOutput);
+    if (targetPosition > Constants.ElevatorConstants.MAX_POSITION || targetPosition < Constants.ElevatorConstants.MIN_POSITION) {
+      SmartDashboard.putString("Elevator Error", "Target position out of range!");
+      return;
+    }
+  }
+
+  public void move(double power){
     motor1.set(power);
   }
 
+  public boolean atTargetPosition() {
+    return pidController.atSetpoint();
+  }
+  
+
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("PID Error", pidController.getPositionError());
+    SmartDashboard.putNumber("Elevator Target Position", pidController.getSetpoint());
   }
 }
